6af87aa9825fed209426579781f72b61d7cec0d1
simeiyu <qiushui.smy@xuelangyun.com>
2025-09-05 15:27:54 +0800

    tmp

--- src/views/nishui/right/FlowPane.vue
+++ src/views/nishui/right/FlowPane.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
+import * as echarts from 'echarts';
+
 const active = ref(0)
+
+const chartRef = ref(null);
+const option = {}
+
+let chartline: echarts.ECharts;
+function onResize() {
+  chartline.resize();
+}
+onMounted(() => {
+  if (chartRef.value) {
+    chartline = echarts.init(chartRef.value);
+    chartline.setOption(option);
+    window.addEventListener('resize',  onResize, false);
+  }
+})
 </script>
 
 <template>
-  <el-radio-group v-model="active" size="small">
+  <el-radio-group class="flow-radio" v-model="active" size="small">
     <el-radio-button :value="0">进浆流量</el-radio-button>
     <el-radio-button :value="1">排浆流量</el-radio-button>
   </el-radio-group>
   <div class="flow-line"></div>
 </template>
+
+<style scoped lang="scss">
+.flow-radio {
+  margin-top: 8px;
+}
+
+.flow-line {
+  height: 182px;
+  background-color: #fff;
+  margin-top: 10px;
+}
+</style>
