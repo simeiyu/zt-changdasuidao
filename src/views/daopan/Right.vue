b44f5eb43515551d8d0883e5e12a4402951a1fea
simeiyu <qiushui.smy@xuelangyun.com>
2025-10-16 11:21:52 +0800

    调试

--- src/views/daopan/Right.vue
+++ src/views/daopan/Right.vue
@@ -3,30 +3,7 @@ import { filter, find, forEach, map, toNumber } from 'lodash';
 import AlarmPane from '~/components/AlarmPane.vue';
 import socket, { state } from '~/socket';
 
-const active = ref('0');
-const type = ref('前腔-1#压力测');
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
-  },
-  {
-    value: 'Option2',
-    label: 'Option2',
-  },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-]
+const active = ref('1');
 const color = ["#0084FF", "#F53F3F", "#FAAD14", "#00B42A"]
 const collects = [
   {label: '今日报警', value: 2 },
@@ -48,24 +25,14 @@ const tableData = [
   }
 ]
 
-const dimensions = ['time', '进排浆流量差', '上限', '下限'];
+const dimensions = ref(['time', '进排浆流量差', '上限', '下限']);
 const dimensions2 = ['time', '实测', '最大设定值'];
 
-const source1 = [
-  ['12:00', 120, 120, 130],
-  ['12:01', 80, 120, 80],
-  ['12:02', 150, 120, 130],
-  ['12:03', 120, 120, 130],
-  ['12:04', 100, 120, 130],
-  ['12:05', 110, 120, 130],
-  ['12:06', 120, 120, 130],
-  ['12:07', 150, 120, 130],
-]
-
 const daopanSource = reactive<Record<string, any[]>>({
-  "0": [],
-  "1": [],
-  "2": []
+  "0": [],   // 推进泵流量差
+  "1": [],   // 刀盘总推进力
+  "2": [],   // 刀盘扭矩
+  "3": []    // 贯入度
 })
 
 const handleChange = (value: any) => {
@@ -85,19 +52,20 @@ onMounted(() => {
     handleEmit()
   }) : handleEmit()
   socket.on("series:init", (res: any) => {
-    const { type, const_data } = res
+    const { type, const_data, states } = res
     switch (type) {
       case '刀盘总推进力':
-        daopanSource["0"] = const_data;
-        break;
-      case '刀盘扭矩':
         daopanSource["1"] = const_data;
         break;
-      case '贯入度':
+      case '刀盘扭矩':
         daopanSource["2"] = const_data;
         break;
+      case '贯入度':
+        daopanSource["3"] = const_data;
+        break;
       case '推进泵流量差':
-        console.log('--- series:  init: ', res)
+        daopanSource["0"] = const_data;
+        dimensions.value = ['time', ...states]
         // TODO
         break;
     }
@@ -117,12 +85,12 @@ onUnmounted(() => {
 
 <template>  
   <Title1 class="mg-t mg-l mg-r mg-b-lg">推进泵流量差</Title1>
-  <ChartLines :dimensions="dimensions" :data="source1" :height="220" unit="m 3/h" :color="color" />  
+  <ChartLines :dimensions="dimensions" :data="daopanSource[0]" :height="220" unit="m 3/h" :color="color" />  
   <div class="right-box mg-t mg-b-lg">
     <el-radio-group v-model="active" @change="handleChange">
-      <el-radio-button :value="'0'">刀盘总推进力</el-radio-button>
-      <el-radio-button :value="'1'">刀盘扭矩</el-radio-button>
-      <el-radio-button :value="'2'">贯入度</el-radio-button>
+      <el-radio-button :value="'1'">刀盘总推进力</el-radio-button>
+      <el-radio-button :value="'2'">刀盘扭矩</el-radio-button>
+      <el-radio-button :value="'3'">贯入度</el-radio-button>
     </el-radio-group>
   </div>
   <ChartLines :dimensions="dimensions2" :data="daopanSource[active]" :height="220" unit="m 3/h" :color="color" />
