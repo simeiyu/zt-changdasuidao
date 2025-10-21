1257040f65429bbcc168e7a234637f0dc16a15fb
simeiyu <qiushui.smy@xuelangyun.com>
2025-10-21 17:51:50 +0800

    盾尾密封数据联调

--- src/views/dunwei/Right.vue
+++ src/views/dunwei/Right.vue
@@ -1,34 +1,136 @@
 <script setup lang="ts">
+import * as echarts from 'echarts';
+import { ElMessage } from 'element-plus';
+import { map } from 'lodash';
 import AlarmPane from '~/components/AlarmPane.vue';
 
-const active = ref('前腔-1#压力测');
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
+const chartRef1 = ref<null | HTMLElement>(null);
+const chartRef2 = ref<null | HTMLElement>(null);
+const active = ref('1');
+const source = ref<{
+  [key: string]: {
+    chart_type: string;
+    data_list: Array<any[]>;
+    data_type_name: string;
+    historical_data: Array<any>;
+    name_list: Array<string>;
+  }
+}>({
+  1: {
+    chart_type: 'line',
+    data_list: [[]],
+    data_type_name: '',
+    historical_data: [],
+    name_list: [],    
+  }
+})
+const dianwei = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'];
+const option = {
+  grid: {
+    top: 24,
+    left: 16,
+    right: 16,
+    bottom: 16,
+    containLabel: true,
   },
-  {
-    value: 'Option2',
-    label: 'Option2',
+  legend: {
+    top: 0,
+    left: 8,
+    itemWidth: 8,
+    itemHeight: 4,
+    itemGap: 12,
+    padding: 0,
+    icon: "rect",
+    textStyle: {
+      fontSize: 11,
+      color: "#3D3D3D",
+    },
   },
-  {
-    value: 'Option3',
-    label: 'Option3',
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      lineStyle: {
+        color: "#ABABAB",
+        type: "dashed",
+        width: 1,
+        opacity: 1,
+      },
+    },
+    appendToBody: true,
   },
-  {
-    value: 'Option4',
-    label: 'Option4',
+  xAxis: {
+    type: "value",
+    boundaryGap: false,
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: "#E5E6EB",
+        width: 0.6,
+        type: "solid",
+      },
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      margin: 6,
+      color: "#4E5969",
+      fontSize: 10,
+    },
+    splitLine: {
+      show: true,
+      lineStyle: {
+        color: "#E5E6EB",
+        width: 0.6,
+        opacity: 1,
+        type: "dashed",
+      },
+    },
   },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-]
+  yAxis: {
+    type: "value",
+    scale: true,
+    name: name,
+    nameTextStyle: {
+      fontSize: 10,
+      color: "#828D99",
+      align: "right",
+      verticalAlign: "middle",
+    },
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: "#E5E6EB",
+        width: 0.6,
+      },
+    },
+    axisLabel: {
+      color: "#4E5969",
+      fontSize: 10,
+    },
+    splitLine: {
+      show: true,
+      lineStyle: {
+        color: "#E5E6EB",
+        width: 0.6,
+        opacity: 1,
+        type: "dashed",
+      },
+    },
+  }
+}
+let myChart1: echarts.ECharts;
+let myChart2: echarts.ECharts;
+function onResize() {
+  myChart1.resize();
+  myChart2.resize();
+}
+
 
 const collects = [
-  {label: '今日报警', value: 2 },
-  {label: '本月报警', value: 3 },
-  {label: '累计报警', value: 5 },
+  { label: '今日报警', value: 2 },
+  { label: '本月报警', value: 3 },
+  { label: '累计报警', value: 5 },
 ]
 const tableData = [
   {
@@ -45,28 +147,165 @@ const tableData = [
   }
 ]
 
-const dimensions = ['时间', '实测', '预测', '上限', '下限'];
+let timer: any = null;
+// 盾尾密封
+const getTailShield = async () => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+  try {
+    const res = await fetch(`/getAlgoResult?algoName=tailShield`, {
+      method: 'GET'
+    });
+    const { success, data } = await res.json();
+    if (success) {
+      source.value = data;
+      updateCharts()
+    } else {
+      throw new Error('Failed to fetch algo result');
+    }
+    // timer = setTimeout(() => {
+    //   getTailShield();
+    // }, 1000); // 1 second
+  } catch (error: Error | any) {
+    ElMessage.error('Error fetching algo result:' + error.message);
+  }
+}
 
-const source1 = [
-  ['12:00', 120, 130, 170, 20],
-  ['12:01', 80, 80, 180, 30],
-  ['12:02', 150, 130, 170, 20],
-  ['12:03', 120, 130, 170, 20],
-  ['12:04', 100, 130, 170, 20],
-  ['12:05', 110, 130, 170, 20],
-  ['12:06', 120, 130, 170, 20],
-  ['12:07', 150, 130, 170, 20],
-]
+const getChartHistoricalOption = (data: Array<number[]>) => {
+  return {
+    ...option,    
+    grid: {
+      top: 8,
+      left: 16,
+      right: 16,
+      bottom: 16,
+      containLabel: true,
+    },
+    legend: {
+      show: false,
+    },
+    color: ["#0084FF", "#00B42A", "#F53F3F", "#FAAD14"],
+    dataset: {
+      dimensions: ['index', 'value'],
+      source: map(data, (item, index) => [index, item])
+    },
+    series: {
+      type: "line",
+      name: "历史数据",
+      symbol: "circle",
+      symbolSize: 1,
+      showSymbol: false,
+      smooth: true,
+    }
+  }
+}
+
+const getChartLineOption = (dimensions: string[], data: Array<number[]>) => {
+  return {
+    ...option,
+    legend: {
+      ...option.legend,
+      type: 'scroll',
+    },
+    color: dimensions.length > 4 ? dimensions.map((_, index: number) => dimensions.length - 1 === index ? '#FF7D00' : `rgba(0, 132, 255, ${1 - index * 0.08})`) : ["#0084FF", "#00B42A", "#F53F3F", "#FAAD14"],
+    dataset: {
+      dimensions: ['index'].concat(dimensions),
+      source: [map(data[0], (_, index) => index)].concat(data)
+    },
+    series: dimensions.map((item: string, index: number) => ({
+      type: "line",
+      name: item,
+      symbol: "circle",
+      symbolSize: 1,
+      showSymbol: false,
+      smooth: false,
+      seriesLayoutBy: 'row',
+      encode: {
+        x: 0,
+        y: index + 1,
+        seriesName: index + 1,
+      },
+    }))
+  }
+}
+const getChartScatterOption = (dimensions=['散点', '上限', '当前与右侧点位差值', '下限'], data: Array<number[]>) => {
+  return {
+    ...option,
+    color: ["#0084FF", "#00B42A", "#F53F3F", "#FAAD14"],
+    dataset: {
+      dimensions: ['index'].concat(dimensions),
+      source: data.length > 0 ? [map(data[0], (_, index) => index)].concat(data) : []
+    },
+    series: map(dimensions, (item: string, index: number) => ({
+      type: index === 0 ? 'scatter' : 'line',
+      name: item,
+      seriesLayoutBy: 'row',
+      encode: {
+        x: 0,
+        y: index + 1,
+        seriesName: index + 1,
+      },
+    }))
+  }
+}
+
+const updateCharts = () => {
+  console.log('updateCharts', active.value, source.value[active.value].chart_type);
+  if (chartRef1.value) {
+    if (!myChart1) {
+      myChart1 = echarts.init(chartRef1.value);
+    } else {
+      myChart1.clear();
+    }
+    const option1 = getChartHistoricalOption(source.value[active.value].historical_data);
+    myChart1.setOption(option1)
+  }
+  if (chartRef2.value) {
+    if (!myChart2) {
+      myChart2 = echarts.init(chartRef2.value);
+    } else {
+      myChart2.clear();
+    }
+    const option2 = source.value[active.value].chart_type === 'line' ? getChartLineOption(source.value[active.value].name_list, source.value[active.value].data_list) : getChartScatterOption(source.value[active.value].name_list, source.value[active.value].data_list);
+    myChart2.setOption(option2)
+  }
+}
+
+watch(active, () => {
+  updateCharts();
+})
+
+onMounted(() => {
+  getTailShield()
+  updateCharts();
+  window.addEventListener('resize', onResize, false);
+})
+
+onUnmounted(() => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+})
 </script>
 
-<template>  
-  <Title1 class="mg-t mg-l mg-r">压力高位阶段/注脂阶段</Title1>
+<template>
+  <Title1 class="mg-t mg-l mg-r">{{ source[active].data_type_name }}</Title1>
   <div class="flex flex-end mg-t-lg mg-l mg-r mg-b-lg">
-    <el-select v-model="active" placeholder="请选择" :options="options" style="width: 140px;" />
+    <el-select v-model="active" placeholder="请选择" style="width: 140px;">
+      <el-option v-for="item in dianwei" :key="item" :label="`${item}#`" :value="`${item}`"></el-option>
+    </el-select>
   </div>
-  <ChartLines :dimensions="dimensions" :data="source1" unit="m 3/h"/>
-  <ChartLines :dimensions="dimensions" :data="source1" unit="m 3/h"/>
-  <ChartLines :dimensions="dimensions" :data="source1" unit="m 3/h"/>
+  <div class="chart-line" ref="chartRef1"></div>
+  <div class="chart-line" ref="chartRef2"></div>
   <Title1 class="mg-t mg-l mg-r">预测报警</Title1>
   <AlarmPane :collects="collects" :data="tableData" :height="200" />
 </template>
+
+<style scoped lang="scss">
+.chart-line {
+  width: 344px;
+  height: 200px;
+  margin-top: 10px;
+}
+</style>
